0c6a4d44b4664f430d490d59472e4632a76103ec
gertjan <gkok@vsl.nl>
2020-11-04 15:34:01 +0100

    new2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,9 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0,os.path.abspath('../..'))
-#sys.path.insert(0, os.path.abspath('../../..'))
+sys.path.insert(0, os.path.abspath('../../..'))
 
 # -- Project information -----------------------------------------------------
 
