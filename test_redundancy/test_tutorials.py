3cd5fa655d8b286d65a0a5822bb161f0b3a8bbd6
Anupam Prasad Vedurmudi <anupam.vedurmudi@ptb.de>
2021-03-26 14:56:42 +0100

    fixed failing tests. new method names. file formatting

--- test_redundancy/test_tutorials.py
+++ test_redundancy/test_tutorials.py
@@ -5,5 +5,6 @@ from Met4FoF_redundancy_tutorials.redundancyAgents_tutorial_2 import demonstrate
 def test_execution_of_tutorial_1():
     tut1().shutdown()
 
+
 def test_execution_of_tutorial_2():
     tut2().shutdown()
